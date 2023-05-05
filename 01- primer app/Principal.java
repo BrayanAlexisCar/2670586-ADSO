30f5799e5ee76754e03dba8013c852b96ce7255e
INSTRUCTOR <INSTRUCTOR@PERAPRCSAFSD533.FormacionSena.RED>
2023-05-05 17:42:53 -0500

    primer prueba de Java

---
+++ 01- primer app/Principal.java
@@ -0,0 +1,26 @@
+public class Principal{
+    public static void main(String [] args){
+
+        System.out.println("++--------------------------------++");
+        System.out.println("++---- SISTEMA DE FACTURACION ----++");
+        System.out.println("++--------------------------------++");
+        System.out.println("||                    clientes:89 ||");
+        System.out.println("||                   vendedores:13||");
+        System.out.println("||                   productos:130||");
+        System.out.println("||                    facturas:30 ||");
+        System.out.println("||  1. creacon de cliente         ||");
+        System.out.println("||  2. creacion de vendedor       ||");
+        System.out.println("||  3. creacion de factura        ||");
+        System.out.println("||  4. ver lista de clientes      ||");
+        System.out.println("||  5. ver lista de vendedores    ||");
+        System.out.println("||  6. ver lista de facturas      ||");
+        System.out.println("||  7. ver detalle de factura     ||");
+        System.out.println("||  8. salir                      ||");
+        System.out.println("||                                ||");
+        System.out.println("++--------------------------------++");
+        System.out.println("++--------------------------------++");
+
+}
+
+
+}
